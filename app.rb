e7bbe35d80114154cae8944050f3dc7808b0c5f5
César Salazar <i.am@cesarsalzar.mx>
2011-01-11 02:39:38 -0600

    code cleanup

--- app.rb
+++ app.rb
@@ -9,20 +9,20 @@ require 'dm-migrations'
 require 'dm-timestamps'
 
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/customer-admin.sqlite3")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/shdhers.sqlite3")
 
 class User
   
   include DataMapper::Resource
   
-  property :username,         Text,     :key => true
-  property :first_name,       Text,     :required => true
-  property :last_name,        Text,     :required => true
-  property :email,            Text,     :format => :email_address, :unique => true    
-  property :twitter,          Text
-  property :github,           Text
-  property :expertise,        Text
-  property :interests,        Text
+  property :username,         String,     :key => true
+  property :first_name,       String,     :required => true
+  property :last_name,        String,     :required => true
+  property :email,            String,     :format => :email_address,  :unique => true    
+  property :twitter,          String
+  property :github,           String
+  property :expertise,        String
+  property :interests,        String
   property :created_at,       DateTime
   property :created_at,       DateTime
   property :updated_at,       DateTime
@@ -31,17 +31,26 @@ end
 
 DataMapper.auto_upgrade!
 
+
+#Error handling
+not_found do
+  haml :e404
+end
+
+
+# Home
 get '/' do
   haml :index
 end
 
+
+# Create
 get '/new' do
   haml :new
 end
 
 post '/new' do
-  @user = User.new( params[:user] )
-
+  @user = User.new(params[:user])
   if @user.save
     redirect "/#{@user.username}" 
   else
@@ -49,11 +58,15 @@ post '/new' do
   end
 end
 
+
+#Show all
 get '/list' do
   @users = User.all
   haml :list
 end
 
+
+#Update
 get '/:username/edit' do
   @user = User.get(params[:username])
   haml :edit
@@ -61,22 +74,17 @@ end
 
 post '/:username/edit' do
   @user = User.first(params[:username])
-  if @user.update(  :first_name => params[:first_name],
-                    :last_name => params[:last_name],
-                    :username => params[:username],
-                    :email => params[:email],
-                    :twitter => params[:twitter],
-                    :github => params[:github],
-                    :expertise => params[:expertise],
-                    :interests => params[:interests] )
+  if @user.update( params[:user] )
     redirect "/#{@user.username}/edit"
   else
     redirect "/#{@user.username}"
   end
 end
 
+
+#Delete
 post '/:username/delete' do
-  @user = User.first(params[:username])
+  @user = User.get(params[:username])
   if @user.destroy
     redirect "/list"
   else
@@ -85,15 +93,18 @@ post '/:username/delete' do
 end
 
 
-get '/stylesheets/global.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  sass :global
-end
-
+#Show single
 get '/:username' do
   if @user = User.get(params[:username])
     haml :show
   else
-    halt 404, "There's no user with such username"
+    halt 404
   end
+end
+
+
+#Styles
+get '/stylesheets/global.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :global
 end
